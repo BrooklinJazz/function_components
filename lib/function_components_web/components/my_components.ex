3734c074f13e58d2c1b3d98691364238f811c92d
BrooklinJazz <brooklinjazzmyers@gmail.com>
2024-05-23 16:10:20 -0400

    name attr example

--- lib/function_components_web/components/my_components.ex
+++ lib/function_components_web/components/my_components.ex
@@ -1,6 +1,13 @@
 defmodule FunctionComponentsWeb.MyComponents do
   use LiveViewNative.Component, format: :swiftui
 
+  attr :name, :string, default: "anonymous"
+  def name(assigns) do
+    ~LVN"""
+    <Text>Name: <%= @name%></Text>
+    """
+  end
+
   def my_imported_component(assigns) do
     ~LVN"""
     <Text>Example Imported Component</Text>
